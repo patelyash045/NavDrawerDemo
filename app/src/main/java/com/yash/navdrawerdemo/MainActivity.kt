40fde88400664ec43b26fbf2d95926390fa320c3
Yash <patelyash045@gmail.com>
2021-10-01 15:52:00 +0530

    Navigation Drawer is Done

---
+++ app/src/main/java/com/yash/navdrawerdemo/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.yash.navdrawerdemo
+
+import android.icu.text.CaseMap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
+    lateinit var drawerLayout : DrawerLayout
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.drawerLayout)
+        val navView : NavigationView = findViewById(R.id.nav_View)
+
+
+        toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+
+            it.isChecked = true
+
+            when(it.itemId){
+
+                R.id.menu_home -> replaceFragment(HomeFragment(),it.title.toString())//Toast.makeText(applicationContext,"Clicked Home",Toast.LENGTH_SHORT).show()
+                R.id.menu_message -> replaceFragment(MessageFragment(),it.title.toString())//Toast.makeText(applicationContext,"Clicked Message",Toast.LENGTH_SHORT).show()
+                R.id.menu_sync -> Toast.makeText(applicationContext,"Sync Data",Toast.LENGTH_SHORT).show()
+                R.id.menu_trash -> replaceFragment(TrashFragment(),it.title.toString())//Toast.makeText(applicationContext,"Clicked Trash",Toast.LENGTH_SHORT).show()
+                R.id.menu_settings -> replaceFragment(SettingsFragment(),it.title.toString()) //Toast.makeText(applicationContext,"Clicked Setting",Toast.LENGTH_SHORT).show()
+                R.id.menu_login -> replaceFragment(LoginFragment(),it.title.toString())//Toast.makeText(applicationContext,"Clicked Login",Toast.LENGTH_SHORT).show()
+                R.id.menu_share -> Toast.makeText(applicationContext,"Clicked Share",Toast.LENGTH_SHORT).show()
+                R.id.menu_rate_us -> Toast.makeText(applicationContext,"Clicked Rate us",Toast.LENGTH_SHORT).show()
+            }
+
+            true
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment, title: String){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout,fragment)
+        fragmentTransaction.commit()
+        drawerLayout.closeDrawers()
+        setTitle(title)
+    }
+
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+}
