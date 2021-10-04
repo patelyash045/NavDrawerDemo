887f5b5c213427275f13da50cb2e49e3c2edd3ea
Yash <patelyash045@gmail.com>
2021-10-04 13:08:17 +0530

    add adapter class

---
+++ app/src/main/java/com/yash/navdrawerdemo/adapter/HomeAdapter.kt
@@ -0,0 +1,37 @@
+package com.yash.navdrawerdemo.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.recyclerview.widget.RecyclerView
+import com.yash.navdrawerdemo.R
+import com.yash.navdrawerdemo.data.Song
+
+class HomeAdapter(val songs: List<Song>) : RecyclerView.Adapter<HomeAdapter.HomeViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
+        val inflater : LayoutInflater = LayoutInflater.from(parent.context)
+        val view : View = inflater.inflate(R.layout.item_view,parent,false)
+        return HomeViewHolder(view)
+    }
+
+
+    override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
+        holder.txtTitle.text = songs[position].title
+        holder.txtDescription.text = songs[position].description
+
+    }
+
+    override fun getItemCount(): Int {
+        return songs.size
+    }
+
+    class HomeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        var txtTitle = itemView.findViewById<TextView>(R.id.txtTitle)
+        var txtDescription = itemView.findViewById<TextView>(R.id.txtDescription)
+        var container = itemView.findViewById<LinearLayout>(R.id.container)
+    }
+}
